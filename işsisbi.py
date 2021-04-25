fcdf5df2f7b19e1c63c674e006fceae3840d337a
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-04-25 18:09:59 +0300

    Add files via upload

---
+++ işsisbi.py
@@ -0,0 +1,26 @@
+import hashlib
+print("""  
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++            git hub : https://github.com/ka4ir -> MD5          +
++                                                               +
++         git hub : https://github.com/hawk-unity? -> SHA512    +
++                                                               +
++                      1 - MD5 CRYPT                            + 
++                                                               +
++                      2  - SHA 512 CRYPT                       +                 
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+""")
+seçim = input("Seçim yapınız: ")
+if seçim == "1":
+    m=hashlib.sha512()
+    sha512 = input("STRİNG : ")
+    m.update(sha512.encode('utf-8'))
+    print(m.hexdigest())
+elif seçim == "2":
+        sifreleyici = hashlib.md5()
+        metin = input("lütfen Hashlenecek metini giriniz: ")
+        sifreleyici.update(metin.encode("utf-8"))
+        hash = sifreleyici.hexdigest()
+        print("Çıktı>>> %s" % hash)
+     
