538d4d1d1d97c2f35a780d15cb90dd1c384a4fec
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-05-22 15:21:43 +0300

    Update işsisbi.py

--- işsisbi.py
+++ işsisbi.py
@@ -15,37 +15,39 @@ print("""
                 5 - SHA 384 CRYPT
                 6 - MD 4 CRYPT
 """)
-seçim = input("Seçim yapınız: ")
-if seçim == "1":
-    m=hashlib.sha512()
-    sha512 = input("STRİNG :")
-    m.update(sha512.encode('utf-8'))
-    print(m.hexdigest())
-elif seçim == "2":
-    sifreleyici = hashlib.md5()
-    metin = input("lütfen Hashlenecek metini giriniz:")
-    sifreleyici.update(metin.encode("utf-8"))
-    hash = sifreleyici.hexdigest()
-    print("Çıktı>>> %s" % hash)
-elif seçim =="3":
-    me=hashlib.sha256()
-    sha256 = input("STRİNG :")
-    me.update(sha256.encode('utf-8'))
-    print(me.hexdigest())
-elif seçim =="4":
-    me=hashlib.sha224()
-    sha224 = input("STRİNG :")
-    me.update(sha224.encode('utf-8'))
-    print(me.hexdigest())
-elif seçim == "5":
-    me=hashlib.sha384()
-    sha384 = input("STRİNG :")
-    me.update(sha384.encode('utf-8'))
-    print(me.hexdigest())
-elif seçim == "6":
-    me=hashlib.md4()
-    md4 = input("STRİNG :")
-    me.update(md4.encode('utf-8'))
-    print(me.hexdigest())
+while True : 
+
+    seçim = input("Seçim yapınız: ")
+    if seçim == "1":
+        m=hashlib.sha512()
+        sha512 = input("STRİNG :")
+        m.update(sha512.encode('utf-8'))
+        print(m.hexdigest())
+    elif seçim == "2":
+        sifreleyici = hashlib.md5()
+        metin = input("lütfen Hashlenecek metini giriniz:")
+        sifreleyici.update(metin.encode("utf-8"))
+        hash = sifreleyici.hexdigest()
+        print("Çıktı>>> %s" % hash)
+    elif seçim =="3":
+        me=hashlib.sha256()
+        sha256 = input("STRİNG :")
+        me.update(sha256.encode('utf-8'))
+        print(me.hexdigest())
+    elif seçim =="4":
+        me=hashlib.sha224()
+        sha224 = input("STRİNG :")
+        me.update(sha224.encode('utf-8'))
+        print(me.hexdigest())
+    elif seçim == "5":
+        me=hashlib.sha384()
+        sha384 = input("STRİNG :")
+        me.update(sha384.encode('utf-8'))
+        print(me.hexdigest())
+    elif seçim == "6":
+        me=hashlib.md4()
+        md4 = input("STRİNG :")
+        me.update(md4.encode('utf-8'))
+        print(me.hexdigest())
 
      
